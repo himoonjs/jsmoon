280aa9e2a22b0b3c0a7004040e21b14ecebc4cff
hattuni <himoonjs@gmail.com>
2015-04-21 15:03:07 +0300

    First FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_homepage(self):
+        # User goes to check Moon's homepage
+        self.browser.get('http://localhost:8000')
+
+        # User notices the page title and header mention Jungsoo Moon
+        self.assertIn('Jungsoo Moon', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
