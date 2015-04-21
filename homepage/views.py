371976a192cc0b2e6c3ecf50f57857bce1420fdb
hattuni <himoonjs@gmail.com>
2015-04-21 16:52:37 +0300

    Front page HTML now generated from a template

--- homepage/views.py
+++ homepage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'homepage/home.html')
