64ae57679e98e2d5f151ba45312d46f688daf431
Jungsoo Moon <himoonjs@gmail.com>
2015-04-24 23:01:33 +0300

    move about.html, home.html to same folder as base.html

--- homepage/views.py
+++ homepage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'homepage/home.html')
+    return render(request, 'home.html')
 
 def about_page(request):
-    return render(request, 'homepage/about.html')
+    return render(request, 'about.html')
