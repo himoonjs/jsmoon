f5a918027c279d40bbb6659916ddfe12b9415152
Jungsoo Moon <himoonjs@gmail.com>
2015-04-25 00:48:48 +0300

    add urls.py to homepage app

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'homepage.views.home_page', name='home'),
-    url(r'^homepage/', include('homepage.urls')),
+    url(r'^about/', 'homepage.views.about_page', name='about'),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
