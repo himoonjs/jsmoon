30461cf46749298e96c1e08a2beb9233fc3e9611
Jungsoo Moon <himoonjs@gmail.com>
2015-04-24 23:17:38 +0300

    change templates' names

--- homepage/views.py
+++ homepage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'template_home.html')
 
 def about_page(request):
-    return render(request, 'about.html')
+    return render(request, 'template_about.html')
