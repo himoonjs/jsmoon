416d2cd4ffc249c2e48ca4f32f56f8ec098aa9cb
Jungsoo Moon <himoonjs@gmail.com>
2015-04-25 11:47:55 +0300

    rollback to end of provisioning

--- homepage/views.py
+++ homepage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'template_home.html')
+    return render(request, 'homepage/home.html')
 
 def about_page(request):
-    return render(request, 'template_about.html')
+    return render(request, 'homepage/about.html')
