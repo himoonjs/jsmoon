5f3b5de01ddffef24768912d2f35be4c1f384da2
Jungsoo Moon <himoonjs@gmail.com>
2015-06-09 21:29:19 +0300

    homepage under dev staging test

--- homepage/views.py
+++ homepage/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 def home_page(request):
     return render(request, 'homepage/home.html')
 
-
-def about_page(request):
-    return render(request, 'homepage/about.html')
