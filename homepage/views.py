b552a5ad372d5673e749b291a319d0626a3b5a98
Jungsoo Moon <himoonjs@gmail.com>
2015-06-21 00:01:49 +0300

    plus button added

--- homepage/views.py
+++ homepage/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 def home_page(request):
     return render(request, 'homepage/home.html')
 
+
 def lettertolamia_page(request):
     return render(request, 'homepage/lettertolamia.html')
-
