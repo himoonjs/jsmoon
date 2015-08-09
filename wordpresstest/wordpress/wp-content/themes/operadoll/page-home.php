38f90ec69fc7214ee4101b6beedd55ee6c1aa5a8
Jungsoo Moon <himoonjs@gmail.com>
2015-08-09 17:46:42 +0300

    riku silhouette added to the background

--- wordpresstest/wordpress/wp-content/themes/operadoll/page-home.php
+++ wordpresstest/wordpress/wp-content/themes/operadoll/page-home.php
@@ -8,36 +8,36 @@ Template Name: HOME page template
 
         <div class="section group first_row_container">
             <div class="col span_1_of_3 circle" id="home_first_row_left" style="background-color:none;">
-                <a href="http://localhost:8888/wordpress/our-cats/">
+                <a href="#">
                 <div class="circle_frame" id="circle_frame_our_cats" style="background-image: url(<?php echo site_url(); ?>/wp-content/themes/operadoll/images/ourcats.jpg)"></div>
                 </a>
-                <p class="home_titles" id="home_title_our_cats"><a href="http://localhost:8888/wordpress/our-cats/">Kissani</a></p>
+                <p class="home_titles" id="home_title_our_cats"><a href="#">Kissani</a></p>
             </div>
             <div class="col span_1_of_3 circle" id="home_first_row_middle" style="background-color:none;">
-                <a href="http://localhost:8888/wordpress/current-litters/">
+                <a href="#">
                 <div class="circle_frame" id="circle_frame_current_litters" style="background-image: url(<?php echo site_url(); ?>/wp-content/themes/operadoll/images/current_litters.jpg)"></div>
                 </a>
-                <p class="home_titles" id="home_title_current_litters"><a href="http://localhost:8888/wordpress/current-litters/">Pennut</a></p>
+                <p class="home_titles" id="home_title_current_litters"><a href="#">Pennut</a></p>
             </div>
             <div class="col span_1_of_3 circle" id="home_first_row_right" style="background-color:none;">
-                <a href="http://localhost:8888/wordpress/archives/">
+                <a href="#">
                 <div class="circle_frame" id="circle_frame_previous_litters" style="background-image: url(<?php echo site_url(); ?>/wp-content/themes/operadoll/images/previous_litters.jpg)"></div>
                 </a>
-                <p class="home_titles" id="home_title_previous_litters"><a href="http://localhost:8888/wordpress/archives/">Edelliset Pentueet</a></p>
+                <p class="home_titles" id="home_title_previous_litters"><a href="#">Edelliset Pentueet</a></p>
             </div>
         </div>
         <div class="section group second_row_container">
             <div class="col span_1_of_2 circle circle_second_row" id="home_second_row_left" style="background-color:none;">
-                <a href="http://localhost:8888/wordpress/ragdoll/">
+                <a href="#">
                 <div class="circle_frame" id="circle_frame_ragdolls" style="background-image: url(<?php echo site_url(); ?>/wp-content/themes/operadoll/images/ragdoll.jpg)"></div>
                 </a>
-                <p class="home_titles" id="home_title_ragdolls"><a href="http://localhost:8888/wordpress/ragdoll/">Ragdolls</a></p>
+                <p class="home_titles" id="home_title_ragdolls"><a href="#">Ragdolls</a></p>
             </div>
             <div class="col span_1_of_2 circle circle_second_row" id="home_second_row_right" style="background-color:none;">
-                <a href="http://localhost:8888/wordpress/american-curls/">
+                <a href="#">
                 <div class="circle_frame" id="circle_frame_american_curls" style="background-image: url(<?php echo site_url(); ?>/wp-content/themes/operadoll/images/american_curl.jpg)"></div>
                 </a>
-                <p class="home_titles" id="home_title_american_curls"><a href="http://localhost:8888/wordpress/american-curls/">American Curls</a></p>
+                <p class="home_titles" id="home_title_american_curls"><a href="#">American Curls</a></p>
             </div>
         </div>
 
