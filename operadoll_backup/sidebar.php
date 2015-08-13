b756646f2f87f8b609f03d2c6f499f8b24c439c4
Jungsoo Moon <himoonjs@gmail.com>
2015-08-13 09:29:21 +0300

    wp files removed

--- operadoll_backup/sidebar.php
+++
@@ -1,8 +0,0 @@
-            <div class="col span_1_of_3" id="main_side_content" style="background-color:blue;">
-                <em>Current Litters</em>
-                <?php if ( is_active_sidebar( 'sidebar-widget-area' ) ) : ?>
-                  <div>   
-                    <?php dynamic_sidebar( 'sidebar-widget-area' ); ?>
-                  </div>
-                <?php endif; ?>
-            </div>
